55449da0535558f099be3e5da687ca69f6479c51
maestro-de-codewars <37005302+maestro-de-codewars@users.noreply.github.com>
2018-03-02 21:34:56 -0400

    Patada Inicial. Que comience el partido.

---
+++ (do not) README.txt
@@ -0,0 +1,18 @@
+Algo hecho rapido para saber que hacer en la practica.
+
+Hasta ahora es una aplicacion de consola, pero estoy pensando que seria mejor como una libreria.
+Don't Worry, Be Happy.
+
+- login (independiente, validacion = año, *obligatorio estar logueado para correr los otros comandos, guardar info en variables en el codigo)
+(Funcion 1): login(StudentId = "string")
+
+- codewars (toma la info de los ejercicios, la compara con la info de codewars, crea un nuevo CSV y (guarda un archivo nuevo con el nombre dado y lo printea a la consola)
+(Funcion 2): codewars(code_wars = "codewars.csv", ejercicios = "ejercicios.csv", nombre_para_el_archivo = "processed.csv")
+
+- summary (leer solo el CSV de codewars(), hacer los diferentes conteos/calculos y printear la info)
+(Funcion 3): summary()
+
+1-Numero 1: leer y escribir a los archivos.
+2-Federico: summary()
+3-Snow: login()
+4-Bidó: codewars()
---
+++ pau.py
@@ -0,0 +1,47 @@
+# Cada uno tiene sus partes pero,
+# si crees que puedes ayudar a uno de tus compañeros
+# no dudes en hacerlo.
+
+# Al final sera "False" pero
+# por ahora sera "True"
+# para poder probar el codigo:
+logueado = True
+
+# Esta es la parte de Snow:
+def login(matricula):
+
+
+# Esta es la parte de Bidó
+def codewars(codewars = "codewars.csv", ejercicios = "ejercicios.csv", procesado = "processed.csv"):
+    if logueado:
+        # Escribir codigo aqui
+        # Aqui va codigo de Numero 1
+    else:
+        print("Tienes que loguearte primero.")
+
+# Esta es la parte de Federico:
+def summary():
+    if logueado:
+        # Escribir codigo aqui
+        # Aqui va codigo de Numero 1
+    else:
+        print("Tienes que loguearte primero.")
+
+# Esto es lo que vuelve nuestro codigo
+# una aplicacion:
+while True:
+
+    # Printear las instrucciones. (Hacer)
+    print('''
+Bienvenido.
+''')
+
+    # Primero recibimos un comando: 
+    comando = input().lower
+
+    # Luego vemos que dice el comando y corremos el codigo:
+    # Escribir todos los comandos. (Hacer)
+    if comando == "exit":
+        exit()
+
+# I hope this has been enough boilerplate ^.^
