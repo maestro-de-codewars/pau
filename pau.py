d8944c988272e8cbcdb910464ce8f59f193677b3
maestro-de-codewars <37005302+maestro-de-codewars@users.noreply.github.com>
2018-03-04 13:33:27 -0400

    Some minor changes. Y la parte que me toca avanzada.

--- pau.py
+++ pau.py
@@ -7,25 +7,61 @@
 # para poder probar el codigo:
 logueado = True
 
+# Por ahora sera "12345678"
+# para poder probar el codigo:
+student_id = "12345678"
+
 # Esta es la parte de Snow:
 def login(matricula):
-    logueado = False
+    logueado = True
+    student_id = matricula
 
 # Esta es la parte de Bidó
 def codewars(code_wars = "codewars.csv", ejercicios = "ejercicios.csv", procesado = "processed.csv"):
     if logueado:
-        print(code_wars)
-        # Escribir codigo aqui
         # Aqui va codigo de Numero 1
+        # Escribir codigo aqui
+        from datetime import datetime
+        archivo_ejercicios = []
+        file.readlines()
+        archivo_ejercicios.append()
+        ejercicios = []
+        hechos = {}
+        for item in archivo_codewars:
+            hechos.[item[1]] = item[4]
+
+        for item in archivo_ejercicios:
+            if item[2] in hechos.keys():
+                date_completed = datetime.strptime(hechos[item[2]], "%Y-%m-%dT%H:%M:%SZ")
+                due_date = datetime.strptime(item[1], "%Y,%m,%d")
+                check = date_completed > due_date
+                ejercicios.append([item[0], item[2], True, hechos[item[2]], check])
+            else:
+                
+
+        return ()
+        # Retorna:
+        # Batch (string), Exercise (string),  Completed (bool), DateCompleted(String or None), CompletedLate (bool)
+
     else:
         print("Tienes que loguearte primero.")
 
 # Esta es la parte de Federico:
 def summary():
     if logueado:
-        print(logueado)
-        # Escribir codigo aqui
         # Aqui va codigo de Numero 1
+
+        # Escribir codigo aqui
+
+        print (" StudentId: %s /n TotalExcercises: %i /n TotalCompleted: %i /n TotalLate: %i /n TotalMissing: %i" % (student_id, t_exercices, t_completed, t_late, t_missing))
+        # Genera el siguiente reporte en consola:
+        # StudentId: Matricula actualmente loggeada.
+        # TotalExcercises: (int)
+        # TotalCompleted: (int)
+        # TotalLate: (int)
+        # TotalMissing: (int)
+
+        # Nota para Federico: Ponle el nombre que quieras a las variables. O trabaja con ellas asi. Your choice.
     else:
         print("Tienes que loguearte primero.")
 
@@ -38,7 +74,7 @@ while True:
 Bienvenido.
 ''')
 
-    # Primero recibimos un comando: 
+    # Primero recibimos un comando:
     comando = input().lower()
 
     # Luego vemos que dice el comando y corremos el codigo:
