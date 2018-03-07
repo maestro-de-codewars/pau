358304eff37bf17c1571ac9189541323c3a51faf
maestro-de-codewars <37005302+maestro-de-codewars@users.noreply.github.com>
2018-03-07 02:23:18 -0400

    Pre-Pull preparation

--- pau.py
+++ pau.py
@@ -25,29 +25,44 @@ def login():
 
 
 # Esta es la parte de Bidó
-def codewars(code_wars = "codewars.csv", ejercicios = "ejercicios.csv", procesado = "processed.csv"):
+def codewars():
 
-    print("Ingrese ")
-    entrada2 = input()
-    print("Ingrese ")
-    entrada3 = input()
-    print("Ingrese ")
-    entrada4 = input()
+    print("Ingrese el path del archivo del cronograma de ejercicios:")
+    ejercicios = input()
+    print("Ingrese el path del archivo de Codewars:")
+    code_wars = input()
+    print("Ingrese el nombre con el que quiere salvar el archivo procesado:")
+    procesado = input()
+
+    if ejercicios == "":
+        ejercicios = "exercises.csv"
+
+    if code_wars == "":
+        code_wars = "codewars.csv"
+
+    if procesado == "":
+        procesado = "processed.csv"
+
+    lista_de_inputs = [ejercicios, code_wars, procesado]
+
+    for item in lista_de_inputs:
+        if not item[-4:] == ".csv":
+            print("Tienes que escribir los nombres de todos los archivos terminando en .csv")
+            print("Intenta denuevo.")
+            return
 
     # Aqui va codigo de Numero 1
     path1 = ejercicios
     archivo_ejercicios = []
     with open (path1,"r") as file:
         for item in file.readlines():
-            archivo_ejercicios.append(item.replace("https://www.codewars.com/kata/","").split(","))
+            archivo_ejercicios.append(item.replace("https://www.codewars.com/kata/","").strip().split(","))
 
     path2 = code_wars
     archivo_codewars = []
     with open (path2,"r") as archivo:
         for line in archivo.readlines():
-            archivo_codewars.append(line.split(","))
-
-
+            archivo_codewars.append(line.strip().split(","))
 
     # Este es el algoritmo:
     # 1- Crear un diccionario para guardar la informacion de los ejercicios hechos en Codewars.
@@ -84,7 +99,7 @@ def codewars(code_wars = "codewars.csv", ejercicios = "ejercicios.csv", procesad
 
             lista_ejercicios.append([item2[0], item2[3], True, hechos[item2[3]], check])
                 # Aqui le agregamos una lista a la lista 'ejercicios'. Con el formato adecuado (See below).
-
+                
         else:
             lista_ejercicios.append([item2[0], item2[3], False, None, False])
             # Aqui le agregamos una lista a la lista 'ejercicios'. Con el formato adecuado (See below).
@@ -93,26 +108,31 @@ def codewars(code_wars = "codewars.csv", ejercicios = "ejercicios.csv", procesad
         # Batch (string), Exercise (string),  Completed (bool), DateCompleted(String or None), CompletedLate (bool)
 
     # Aqui va el codigo para escribir 'ejercicios' al CSV.
+    # with
 
     # Aqui continua el codigo que falta. (Retornar la informacion a la consola.)
     for item3 in lista_ejercicios:
         print("%s, %s, %s, %s, %s" % (item3[0], item3[1], item3[2], item3[3], item3[4]))
 
 # Esta es la parte de Federico:
-def summary(list_):
+def summary():
+
+    # Aqui va el codigo para leer el archivo procesado.
+    # retorna una lista de listas en la variable "archivo_procesado"
+
     #nota: la lista debe estar en el formato pedido por el profe que es:
 # list= [Batch (string), Exercise (string),  Completed (bool), DateCompleted(String or None), CompletedLate (bool)]
 #cualquier duda ver la lista de prueba adjuntada abajo
     Total_Completed = 0   # se declara la variable
     Total_Late = 0      #se declara la variable
-    Total_Excercises = len(list_)    # esta variable sera igual a la cantidad de listas que tenga la lista ya que por cada lista habra un ejercicio
-    for exercise in list_:           #vamos a recorrer cada lista para sacar la data
+    Total_Excercises = len(archivo_procesado)    # esta variable sera igual a la cantidad de listas que tenga la lista ya que por cada lista habra un ejercicio
+    for exercise in archivo_procesado:           #vamos a recorrer cada lista para sacar la data
         if exercise[2] == True:     #verificamos si el ejercicio fue hecho
             Total_Completed +=1
             if exercise[4] == True:     #verificamos si se entrego tarde
                 Total_Late +=1
-    Total_Missing = len(list_) - Total_Completed   # los ejercicio no entregados son igual al total de ejercicios menos los realizados
-    return 'report: \nStudentId:%i \nTotalExcercises:%i \nTotalCompleted:%i \nTotalLate:%i \nTotalMissing:%i' %(20157443,Total_Excercises,Total_Completed,Total_Late,Total_Missing )
+    Total_Missing = len(archivo_procesado) - Total_Completed   # los ejercicio no entregados son igual al total de ejercicios menos los realizados
+    print ('report: \nStudentId:%i \nTotalExcercises:%i \nTotalCompleted:%i \nTotalLate:%i \nTotalMissing:%i' %(student_id,Total_Excercises,Total_Completed,Total_Late,Total_Missing))
     # lista de prueba debajo
     #list_=[[1,'ejer1',True,'28/2/2018',True],[2,'ejer2',False,'28/2/2018',False],[3,'ejer3',True,'28/2/2018',False]]
 #print(summary(list_))    #con esto mostramos en pantalla la data de summary
@@ -137,7 +157,12 @@ Comandos:
     if comando1 == "login":
         login()
         while logueado:
-            print("Instrucciones")
+            print('''
+Comandos:
+    - codewars
+    - summary
+    - exit
+''')
             comando2 = input()
             if comando2 == "codewars":
                 codewars()
